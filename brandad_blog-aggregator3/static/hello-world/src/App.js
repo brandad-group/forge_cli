80202c81a346f0791593efcca5ed033c0a8b4b2a
AlexBrandad <alexander.niemczyk@brandad.de>
2024-01-12 03:56:29 +0100

    Kachel design updated, Design Overlay unten nun immer über volle breite

--- brandad_blog-aggregator3/static/hello-world/src/App.js
+++ brandad_blog-aggregator3/static/hello-world/src/App.js
@@ -16,39 +16,42 @@ const loadBlog = async (nextLink, back = false) => {
 };
 
 const renderBlogPostElements = (blogPosts) => {
-  let blogPostElements = [];
-  for (let i = 0; i < blogPosts.length; i++) {
-    let post = blogPosts[i];
-    blogPostElements.push(
-      <div class="blog-container">
-        <h2 class="blog-title">{post.title}</h2>
-        <div class="blog-image-container">
-          <img class="blog-image" alt="Bild" src={post.__image} />
-        </div>
-        <span>{new Date(post.createdAt)?.toLocaleString('de-DE') ?? 'undefined'}</span>
-        <span class="blog-space-name">{spacesCache[post.spaceId]?.name ?? 'undefined'}</span>
-        <a class="blog-link" href={'/wiki' + post._links.webui} target='_blank'>Gehe zum Post</a>
-      </div>
-    );
-  }
-  return blogPostElements;
+    const limitTitle = (title, maxLength) => {
+        return title.length > maxLength ? title.slice(0, maxLength) + '...' : title;
+    };
+    return blogPosts.map((post, index) => (
+        <a className="blog-link" href={'/wiki' + post._links.webui} target='_blank' rel="noopener noreferrer">
+            <div key={index} className="flex-item item">
+                <div className="blog-image-container">
+                    <img className="blog-image" alt="Bild" src={post.__image}/>
+                    <div className="overlay">
+                        <div className="blog-details">
+                            <span className="blog-title">{limitTitle(post.title, 80)}</span>
+                            <span>{new Date(post.createdAt)?.toLocaleString('de-DE') ?? 'undefined'}</span>
+                            <span className="blog-space-name">{spacesCache[post.spaceId]?.name ?? 'undefined'}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </a>
+    ));
 };
 
 function App() {
-  const [blogs, setBlogs] = useState(undefined);
+    const [blogs, setBlogs] = useState(undefined);
 
-  useEffect(async () => {
-    setBlogs(await loadBlog(undefined));
-  }, []);
+    useEffect(async () => {
+        setBlogs(await loadBlog(undefined));
+    }, []);
 
-  return (
-    <div class="text-light">
-      Number of blogposts in this confluence instance: {blogs?.results?.length}
-      <div class="blog-grid">
-        {renderBlogPostElements(blogs?.results ?? [])}
-      </div>
-    </div>
-  );
+    return (
+        <div class="text-light">
+            Number of blogposts in this confluence instance: {blogs?.results?.length}
+            <div class="flex-container">
+                {renderBlogPostElements(blogs?.results ?? [])}
+            </div>
+        </div>
+    );
 }
 
 export default App;
