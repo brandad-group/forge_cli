0c4706fc0ec4f07ea941a005d01e8787c6a6919c
Mark Schoenlein <mark.schoenlein@brandad.de>
2024-01-12 09:28:56 +0100

    fix: blog link
    Also improve design.

--- brandad_blog-aggregator3/static/hello-world/src/App.js
+++ brandad_blog-aggregator3/static/hello-world/src/App.js
@@ -2,38 +2,34 @@ import React, { useEffect, useState } from 'react';
 
 import { fetchBlogPosts, fetchBlogSpace, fetchBlogImage } from './api-service';
 import { spacesCache } from './storage';
+import { router } from '@forge/bridge';
 
 const loadBlog = async (nextLink, back = false) => {
-  const fetchedBlogs = await fetchBlogPosts(nextLink, back);
-  if (fetchedBlogs.results.length > 0) {
-    for (let i = 0; i < fetchedBlogs.results.length; i++) {
-      await fetchBlogSpace(fetchedBlogs.results[i].spaceId);
-      fetchedBlogs.results[i].__image = await fetchBlogImage(fetchedBlogs.results[i].id);
+    const fetchedBlogs = await fetchBlogPosts(nextLink, back);
+    if (fetchedBlogs.results.length > 0) {
+        for (let i = 0; i < fetchedBlogs.results.length; i++) {
+            await fetchBlogSpace(fetchedBlogs.results[i].spaceId);
+            fetchedBlogs.results[i].__image = await fetchBlogImage(fetchedBlogs.results[i].id);
+        }
     }
-  }
 
-  return fetchedBlogs;
+    return fetchedBlogs;
 };
 
 const renderBlogPostElements = (blogPosts) => {
-    const limitTitle = (title, maxLength) => {
-        return title.length > maxLength ? title.slice(0, maxLength) + '...' : title;
-    };
     return blogPosts.map((post, index) => (
-        <a className="blog-link" href={'/wiki' + post._links.webui} target='_blank' rel="noopener noreferrer">
-            <div key={index} className="flex-item item">
-                <div className="blog-image-container">
-                    <img className="blog-image" alt="Bild" src={post.__image}/>
-                    <div className="overlay">
-                        <div className="blog-details">
-                            <span className="blog-title">{limitTitle(post.title, 80)}</span>
-                            <span>{new Date(post.createdAt)?.toLocaleString('de-DE') ?? 'undefined'}</span>
-                            <span className="blog-space-name">{spacesCache[post.spaceId]?.name ?? 'undefined'}</span>
-                        </div>
+        <button className="blog-link" onClick={() => { router.navigate('/wiki' + post._links.webui); }}>
+            <div key={index} className="flex-item">
+                <img className="blog-image" alt="Bild" src={post.__image} />
+                <div className="overlay">
+                    <div className="blog-details">
+                        <span className="blog-title">{post.title}</span>
+                        <span>{new Date(post.createdAt)?.toLocaleString('de-DE') ?? 'undefined'}</span>
+                        <span className="blog-space-name">{spacesCache[post.spaceId]?.name ?? 'undefined'}</span>
                     </div>
                 </div>
             </div>
-        </a>
+        </button>
     ));
 };
 
